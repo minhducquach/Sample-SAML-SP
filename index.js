76135dd17c656ee40036b222ae96f0cbf33d7684
lausamtuong <lausamtuong@gmail.com>
2023-11-06 14:58:11 +0700

    build validate saml assertion

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const auth = require("./saml-config/passport")
 const config = require("./saml-config/config")
 const session = require('express-session')
+var saml = require('saml20');
 
 const app = express()
 const port = 3000
@@ -32,7 +33,40 @@ app.get('/login', auth.authenticate('saml', config.saml.options), (req, res, nex
 })
 
 app.post('/login/success', auth.authenticate('saml', config.saml.options), (req, res, next) => {
-    return res.redirect('/homepage');
+
+    var options = {
+        publicKey: `MIIDqDCCApCgAwIBAgIGAYrr7ImhMA0GCSqGSIb3DQEBCwUAMIGUMQswCQYDVQQGEwJVUzETMBEG
+        A1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzENMAsGA1UECgwET2t0YTEU
+        MBIGA1UECwwLU1NPUHJvdmlkZXIxFTATBgNVBAMMDGRldi0yMzU1MzkxODEcMBoGCSqGSIb3DQEJ
+        ARYNaW5mb0Bva3RhLmNvbTAeFw0yMzEwMDExNTQ2NDJaFw0zMzEwMDExNTQ3NDFaMIGUMQswCQYD
+        VQQGEwJVUzETMBEGA1UECAwKQ2FsaWZvcm5pYTEWMBQGA1UEBwwNU2FuIEZyYW5jaXNjbzENMAsG
+        A1UECgwET2t0YTEUMBIGA1UECwwLU1NPUHJvdmlkZXIxFTATBgNVBAMMDGRldi0yMzU1MzkxODEc
+        MBoGCSqGSIb3DQEJARYNaW5mb0Bva3RhLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
+        ggEBAIkkLlGWkz/3nFccF9NwMXvMMtvlxn31r3Rt/Npnrt9zyn7W1qj3tXC98//roIctzgnpCz1Y
+        bnCNwjGJTUuuSKRDiNohDZ0mR82RTAIOZR71yDUb84ej0RNWV3M1ydshVLH1yJYCQAO1v6jSHI59
+        jewAE6DWumI5XC9JgnB2fdRJtflSG+ZnhaxXA1YlB+0hUdpb6nDPWOB0t9MXMcJh2g0KrDp/Qg1A
+        Olwy0Zk4SkYyBB3pMyPzEmqDqI9iMBBSClmY4zZ1SYdX36hVTuhw8VPXQh9tAKc4ONEc5ycoSFzU
+        CMwaplSrsMDtMiGbJuhLD/Wr9c5m0VzTdk6v9CGP8BcCAwEAATANBgkqhkiG9w0BAQsFAAOCAQEA
+        Z9SunooQknoRuSCBZJb8lvGQlzMTztx5uwJjq7Ihj7D088lMsf/0ElB8Q/OhS+wa8kkwRlnNTyo6
+        Jx+d/24ns3bHf+o1ydjQJJ6Jqyk5fECauzdWsgqzxkkHusN2m1eud0i4KHJOtMUTnOkgMUiWPoi+
+        ESB3H2cU6Ohw4SkkJ7RzpEjRcnAn0XgBzXmN5uuRuZjrnkiyltYMMbr3apk8J4OYF+CAa04Q6kD7
+        +XKoHSzCisJYV8H6YOmCietU9saMXwgA4/vaTvkjo9dNwHQqSMQNGr9E31cVDXfFjldOpc4W7N8z
+        XPLTk5AMIt/NRRZWxtL6BGQD9/vhdjyQHnGfcw==`,
+        audience: 'http://localhost:3000'
+    }
+    
+    const rawAssertion = req.user.getAssertionXml()
+    saml.validate(rawAssertion, options, function(err, profile) {
+        // err
+        if(profile){
+
+            return res.redirect('/homepage');
+        }
+        // var claims = profile.claims; // Array of user attributes;
+        // var issuer = profile.issuer; // String Issuer name.
+    });
+
+    
 })
 
 app.get('/homepage', auth.protected, (req, res) => {
